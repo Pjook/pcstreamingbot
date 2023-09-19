c225ff3ab84ea626a44f3e1456ff34b6fa45d74a
parshucr123 <118430657+parshucr123@users.noreply.github.com>
2023-09-19 17:20:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,7 +112,7 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "    URL = "71ccdf4315be.herokuapp.com".format(FQDN)
+    URL = "    URL =  "71ccdf4315be.herokuapp.com".format(FQDN)
 else:
     URL = "71ccdf4315be.herokuapp.com".format(FQDN)
 
