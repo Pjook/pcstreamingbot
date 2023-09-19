cd798e968653a63411310e1ed9223f701df22114
parshucr123 <118430657+parshucr123@users.noreply.github.com>
2023-09-19 17:08:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "    URL = "mxplaybot-71ccdf4315be.herokuapp.com".format(FQDN)
+    URL = "    URL = "71ccdf4315be.herokuapp.com".format(FQDN)
 else:
-    URL = "mxplaybot-71ccdf4315be.herokuapp.com".format(FQDN)
+    URL = "71ccdf4315be.herokuapp.com".format(FQDN)
 
 
 
