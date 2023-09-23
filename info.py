49d54f0254ef2a6a82aa4172e9bd0f14baa0ff1b
parshucr123 <118430657+parshucr123@users.noreply.github.com>
2023-09-24 00:03:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://mx-play-bot-clf3.onrender.com/".format(FQDN)
+    URL = "streambot123-dd2f85209375.herokuapp.com".format(FQDN)
 else:
-    URL = "https://mx-play-bot-clf3.onrender.com/".format(FQDN)
+    URL = "streambot123-dd2f85209375.herokuapp.com".format(FQDN)
 
 
 
